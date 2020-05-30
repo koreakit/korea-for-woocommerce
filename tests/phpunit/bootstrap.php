135d912037e9a610beda81a8574e09a2e26c8676
GREYS <4136890+jgreys@users.noreply.github.com>
2020-05-30 10:24:58 +0900

    Fix - PHPUnit bootstrap

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -7,21 +7,21 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	$plugin_dir = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/';
-	require $plugin_dir . 'woocommerce/woocommerce.php';
-	require $plugin_dir . 'korea-for-woocommerce/korea-for-woocommerce.php';
+	require dirname( dirname( __FILE__ ) ) . '/korea-for-woocommerce.php';
+	require dirname( dirname( __FILE__ ) ) . '../../woocommerce/woocommerce.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-$wc_tests_framework_base_dir = dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/woocommerce/tests/framework/';
+$wc_tests_framework_base_dir = dirname( dirname( __FILE__ ) ) . '../../woocommerce/tests/framework/';
 require_once $wc_tests_framework_base_dir . 'class-wc-mock-session-handler.php';
-// require_once( $wc_tests_framework_base_dir . 'class-wc-unit-test-case.php' );
+require_once $wc_tests_framework_base_dir . 'class-wc-unit-test-case.php';
 require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-product.php';
 require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-coupon.php';
 require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-fee.php';
 require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-shipping.php';
 require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-customer.php';
 require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-order.php';
+require_once 'class-wc-booking-product-test-helper.php';
