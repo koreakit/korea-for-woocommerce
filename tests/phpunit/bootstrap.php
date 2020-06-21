937e0cfad760eff65967a9729bf704590850b569
GREYS <4136890+jgreys@users.noreply.github.com>
2020-06-21 15:36:23 +0900

    PHPUnit bootstrap file updated

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,27 +1,91 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package WC_KCP
+ */
+
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run tests/bin/install.sh ?" . PHP_EOL; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/korea-for-woocommerce.php';
-	require dirname( dirname( __FILE__ ) ) . '../../woocommerce/woocommerce.php';
+/**
+ * Returns WooCommerce main directory.
+ *
+ * @return string
+ */
+function wc_dir() {
+	return rtrim( sys_get_temp_dir(), '/\\' ) . '/woocommerce';
 }
 
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+/**
+ * Adds WooCommerce testing framework classes.
+ */
+function wc_test_includes() {
+	$wc_tests_framework_base_dir = wc_dir() . '/tests';
+	if ( ! is_dir( $wc_tests_framework_base_dir . '/framework' ) ) {
+		$wc_tests_framework_base_dir .= '/legacy';
+	}
 
+	// WooCommerce test classes.
+	// Framework.
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-mock-session-handler.php';
+	require_once $wc_tests_framework_base_dir . '/framework/class-wc-mock-payment-gateway.php';
+
+	// Helpers.
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-product.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-coupon.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-fee.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-shipping.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-customer.php';
+	require_once $wc_tests_framework_base_dir . '/framework/helpers/class-wc-helper-order.php';
+}
+
+/**
+ * Manually load the plugin being tested.
+ */
+tests_add_filter(
+	'muplugins_loaded',
+	function() {
+		require_once wc_dir() . '/woocommerce.php';
+		require dirname( dirname( dirname( __FILE__ ) ) ) . '/korea-for-woocommerce.php';
+	}
+);
+
+// install WC.
+tests_add_filter(
+	'setup_theme',
+	function() {
+		// Clean existing install first.
+		define( 'WP_UNINSTALL_PLUGIN', true );
+		define( 'WC_REMOVE_ALL_DATA', true );
+		include wc_dir() . '/uninstall.php';
+
+		WC_Install::install();
+
+		// Reload capabilities after install, see https://core.trac.wordpress.org/ticket/28374.
+		if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
+			$GLOBALS['wp_roles']->reinit();
+		} else {
+			$GLOBALS['wp_roles'] = null; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+			wp_roles();
+		}
+
+		echo esc_html( 'Installing WooCommerce...' . PHP_EOL );
+	}
+);
+
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
 
-$wc_tests_framework_base_dir = dirname( dirname( __FILE__ ) ) . '../../woocommerce/tests/framework/';
-require_once $wc_tests_framework_base_dir . 'class-wc-mock-session-handler.php';
-require_once $wc_tests_framework_base_dir . 'class-wc-unit-test-case.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-product.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-coupon.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-fee.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-shipping.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-customer.php';
-require_once $wc_tests_framework_base_dir . 'helpers/class-wc-helper-order.php';
-require_once 'class-wc-booking-product-test-helper.php';
+wc_test_includes();
