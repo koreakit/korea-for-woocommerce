1c436cd7539b9922846ae7db43cb12fcbb311755
GREYS <4136890+jgreys@users.noreply.github.com>
2020-09-01 15:57:08 +0900

    Add tests-setup.sh

---
+++ bin/tests-setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# Exit if any command fails.
+set -e
+
+echo
+echo "Setting up tests environment..."
+echo
+
+echo "Updating WordPress to the latest version..."
+wp-env run tests-cli "wp core update --quiet"
+
+echo "Updating the WordPress database..."
+wp-env run tests-cli "wp core update-db --quiet"
+
+echo "Installing and activating WooCommerce..."
+wp-env run tests-cli "wp plugin install woocommerce --activate"
+
+echo "Adding basic WooCommerce settings..."
+wp-env run tests-cli "wp option set woocommerce_store_address '123 Maple Street'"
+wp-env run tests-cli "wp option set woocommerce_store_city 'Seoul'"
+wp-env run tests-cli "wp option set woocommerce_default_country 'KR'"
+wp-env run tests-cli "wp option set woocommerce_store_postcode '12345'"
+wp-env run tests-cli "wp option set woocommerce_currency 'KRW'"
+wp-env run tests-cli "wp option set woocommerce_product_type 'both'"
+wp-env run tests-cli "wp option set woocommerce_allow_tracking 'no'"
+
+echo "Importing WooCommerce shop pages..."
+wp-env run tests-cli "wp wc --user=admin tool run install_pages"
+
+echo "Installing and activating the WordPress Importer plugin..."
+wp-env run tests-cli "wp plugin install wordpress-importer --activate"
+
+echo "Importing some sample data..."
+wp-env run tests-cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip"
+
+echo "Activating Korea for WooCommerce plugin..."
+wp-env run tests-cli "wp plugin activate korea-for-woocommerce"
+
+echo
+echo "SUCCESS! You should now be able to access http://localhost:8889/wp-admin/"
+echo "Default login credentials are username: admin password: password"
