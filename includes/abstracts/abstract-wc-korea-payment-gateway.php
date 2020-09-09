215850d846dc1a80c9fb690cfef40e6b41aba519
GREYS <4136890+jgreys@users.noreply.github.com>
2020-09-09 17:48:17 +0900

    Fixes installments for payment gateways

--- includes/abstracts/abstract-wc-korea-payment-gateway.php
+++ includes/abstracts/abstract-wc-korea-payment-gateway.php
@@ -77,7 +77,7 @@ abstract class WC_Korea_Payment_Gateway extends WC_Payment_Gateway {
 					<legend class="screen-reader-text"><span><?php echo wp_kses_post( $data['title'] ); ?></span></legend>
 					<select id="<?php echo esc_attr( $field_key ); ?>" class="wc-enhanced-select" <?php echo $is_multiple; ?> name="<?php echo esc_attr( $field_key ); ?>" data-placeholder="<?php echo esc_attr( $data['placeholder'] ); ?>" <?php disabled( $data['disabled'], true ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
 						<?php foreach ( (array) $data['options'] as $option_key => $option_value ) { ?>
-							<option value="<?php echo esc_attr( $option_key ); ?>" <?php echo ( ( in_array( $option_key, explode( ':', $this->get_option( $key ) ), true ) ) ? 'selected="selected"' : '' ); ?>>
+							<option value="<?php echo esc_attr( $option_key ); ?>" <?php echo ( ( in_array( $option_key, explode( ':', $this->get_option( $key ) ) ) ) ? 'selected="selected"' : '' ); // phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict ?>>
 								<?php echo esc_html( $option_value ); ?></option>
 						<?php } ?>
 					</select>
@@ -111,53 +111,51 @@ abstract class WC_Korea_Payment_Gateway extends WC_Payment_Gateway {
 			'options'     => array(),
 		);
 
-		$data  = wp_parse_args( $data, $defaults );
-		$cards = explode( ',', esc_attr( $this->get_option( $key ) ) );
+		$data               = wp_parse_args( $data, $defaults );
+		$value              = explode( ',', esc_attr( $this->get_option( $key ) ) );
+		$noint_installments = array();
+		foreach ( $value as $v ) {
+			list( $cardcode, $installments ) = explode( '-', $v );
+			$noint_installments[ $cardcode ] = $installments;
+		}
 
 		ob_start();
+
 		?>
 		<tr valign="top" class="<?php echo esc_attr( $data['class'] ); ?>">
 			<th scope="row" class="titledesc">
 				<label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?> <?php echo $this->get_tooltip_html( $data ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></label>
 			</th>
 			<td class="forminp">
-				<fieldset>
-					<ul id="<?php echo esc_attr( $field_key ); ?>">
-						<?php
-						$i = 1;
-						foreach ( $cards as $card ) {
-							list($cardcode, $quotabase) = explode( '-', $card );
-							?>
-							<li>
-								<select class="wc-inicis-nointerest-card" name="<?php echo esc_attr( $field_key ); ?>[card][]">
-									<?php foreach ( (array) $data['cards'] as $option_key => $option_value ) { ?>
-										<option value="<?php echo esc_attr( $option_key ); ?>" <?php selected( (string) $option_key, esc_attr( $cardcode ) ); ?>>
-											<?php echo esc_html( $option_value ); ?>
-										</option>
-									<?php } ?>
-								</select>
-
-								<select class="wc-inicis-nointerest-quotabase wc-enhanced-select" name="<?php echo esc_attr( $field_key ); ?>[quotabase][<?php echo esc_attr( $i ); ?>][]" multiple="multiple">
-									<?php foreach ( (array) $data['quotabase'] as $option_key => $option_value ) { ?>
-										<option value="<?php echo esc_attr( $option_key ); ?>" <?php echo ( in_array( $option_key, (array) explode( ':', $quotabase ), true ) ? 'selected="selected"' : '' ); ?>>
-											<?php echo esc_html( $option_value ); ?>	
-										</option>
-									<?php } ?>
-								</select>
-
-								<?php if ( 1 === $i ) { ?>
-									<a class="button-secondary button-nointerest-card-add" href="javascript:;" style="font-weight:bold;">+</a>
-								<?php } else { ?>
-									<a class="button-secondary button-nointerest-card-remove" href="javascript:;" style="color:red; font-weight:bold;">-</a>
-								<?php } ?>
-							</li>
-							<?php
-							++$i;
-						}
-						?>
-					</ul>
-				</fieldset>
 				<?php echo $this->get_description_html( $data ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+				<fieldset>
+					<table>
+						<thead>
+							<tr>
+								<th style="padding:15px 0px"><?php esc_html_e( 'Card company', 'korea-for-woocommerce' ); ?></th>
+								<th style="padding:15px 0px"><?php esc_html_e( 'Interest-free installments', 'korea-for-woocommerce' ); ?></th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach ( $data['cards'] as $cardcode => $cardname ) { ?>
+								<tr>
+									<td width="20%" class="forminp" style="padding:0; margin:0;">
+										<i><?php echo wp_kses_post( wptexturize( $cardname ) ); ?></i>
+									</td>
+									<td width="80%" class="forminp" style="padding:0; margin:0;">
+										<select class="noint-installments wc-enhanced-select" id="<?php echo esc_attr( $field_key . '[' . $cardcode . ']' ); ?>" name="<?php echo esc_attr( $field_key . '[' . $cardcode . '][]' ); ?>" multiple="multiple">
+											<?php foreach ( (array) $data['installments'] as $installment => $label ) { ?>
+												<option value="<?php echo esc_attr( $installment ); ?>" <?php echo isset( $noint_installments[ $cardcode ] ) && in_array( $installment, (array) explode( ':', $noint_installments[ $cardcode ] ) ) ? 'selected="selected"' : ''; // phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict ?>>
+													<?php echo esc_html( $label ); ?>
+												</option>
+											<?php } ?>
+										</select>
+									</td>
+								</tr>
+							<?php } ?>
+						</tbody>
+					</table>
+				</fieldset>
 			</td>
 		</tr>
 		<?php
