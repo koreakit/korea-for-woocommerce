d5046976083864de1d00a2f00e8403d40a46c3b3
GREYS <4136890+jgreys@users.noreply.github.com>
2021-01-02 20:41:59 +0900

    Fixed: Postcode finder is now working for shipping address fields

--- assets/src/js/wc-korea-postcode.js
+++ assets/src/js/wc-korea-postcode.js
@@ -2,74 +2,68 @@ jQuery(function($) {
 	"use strict";
 
 	var wc_korea_postcode = {
-		$body: $( document.body ),
-		$country: $('#billing_country, #shipping_country'),
-		$postcode: $('input#billing_postcode, input#shipping_postcode'),
-		$postcodeForm: $('#postcode_form'),
-		$postcodeClose: $('#postcode_close'),
-		$address1: $('input#billing_address_1, input#shipping_address_1'),
-		$address2: $('input#billing_address_2, input#shipping_address_2'),
-		$city: $('input#billing_city, input#shipping_city'),
-
 		init: function() {
-			wc_korea_postcode.$country.on('change', this.onChange).trigger('change');
-			wc_korea_postcode.$postcode.on('click', this.onClick);
-			wc_korea_postcode.$body.on('click', '#postcode_close', this.onClose);
+			var $body = $( document.body );
+
+			/**
+			 * Korean Address Autocomplete
+			 */
+			$body.find( '#billing_country' ).on( 'change', this.onCountryChange ).trigger( 'change' );
+			$body.find( '#shipping_country' ).on( 'change', this.onCountryChange ).trigger( 'change' );
+			$body.find( '#billing_postcode' ).on( 'click', this.onPostcodeClick );
+			$body.find( '#shipping_postcode' ).on( 'click', this.onPostcodeClick );
+			$body.find( '.address-autocomplete-close' ).on( 'click', function() {
+				$( this ).parent().hide();
+			});
 		},
 
-		onChange: function() {
-			wc_korea_postcode.country = $(this).val();
+		onCountryChange: function() {
+			var $this     = $( this ),
+				$body     = $( document.body ),
+				type      = ( $this.attr( 'name' ).indexOf( 'billing' ) !== -1 ) ? 'billing' : 'shipping',
+				country   = $( '#' + type + '_country' ).val(),
+				$postcode = $( '#' + type + '_postcode' );
 
-			if ( 'KR' !== wc_korea_postcode.country ) {
-				wc_korea_postcode.$postcode.removeAttr('readonly onkeypress');
-				wc_korea_postcode.$body.find('#postcode_form').remove();
+			if ( 'KR' !== country ) {
+				$postcode.removeAttr( 'readonly onkeypress' );
+				$body.find( '#' + type + '-address-autocomplete' ).remove();
 				return;
 			}
 
-			wc_korea_postcode.$postcode.attr({
+			$postcode.attr({
 				'readonly': 'readonly',
 				'onkeypress': 'return false;'
 			});
 
-			wc_korea_postcode.$postcode.closest('p').append('<div id="postcode_form" class="'+ _postcode.theme.displaymode +'" style="display: none;"><img src="//t1.daumcdn.net/postcode/resource/images/close.png" id="postcode_close" style="cursor:pointer; position:absolute; right:0px; top:-1px; z-index:1" alt="접기 버튼"></div>');
+			$postcode.closest( 'p' ).append( '<div id="' + type + '-address-autocomplete" class="'+ type + '-address-autocomplete ' + _postcode.displaymode + '" style="display: none;"><img src="//t1.daumcdn.net/postcode/resource/images/close.png" class="address-autocomplete-close" style="cursor:pointer; position:absolute; right:0px; top:-1px; z-index:1" alt="접기 버튼"></div>' );
 		},
 
-		onClick: function() {
-			if ( 'KR' !== wc_korea_postcode.country ) {
+		onPostcodeClick: function() {
+			var $this   = $( this ),
+				$body   = $( document.body ),
+				type    = ( $this.attr( 'name' ).indexOf( 'billing' ) !== -1 ) ? 'billing' : 'shipping',
+				country = $( '#' + type + '_country' ).val();
+
+			if ( 'KR' !== country ) {
 				return;
 			}
 
 			var daumPostcode = new daum.Postcode({
 				alwaysShowEngAddr: true,
 				hideEngBtn: false,
-				theme: {
-					bgColor          : _postcode.theme.bgcolor,
-					searchBgColor    : _postcode.theme.searchbgcolor,
-					contentBgColor   : _postcode.theme.contentbgcolor,
-					pageBgColor      : _postcode.theme.pagebgcolor,
-					textColor        : _postcode.theme.textcolor,
-					queryTextColor   : _postcode.theme.querytextcolor,
-					postcodeTextColor: _postcode.theme.postcodetextcolor,
-					emphTextColor    : _postcode.theme.emphtextcolor,
-					outlineColor     : _postcode.theme.outlinecolor
-				},
+				theme: _postcode.theme,
 				oncomplete: function(data) {
-					$(document.body).find('#billing_postcode').val(data.zonecode);
-					$(document.body).find('#billing_address_1').val(data.address);
-					$(document.body).find('#billing_address_2').focus();
-					$(document.body).find('#billing_city').val(data.sido);
-
-					wc_korea_postcode.$body.find('#postcode_form').hide();
+					$body.find( '#' + type + '_postcode').val( data.zonecode );
+					$body.find( '#' + type + '_address_1').val( data.address );
+					$body.find( '#' + type + '_address_2').focus();
+					$body.find( '#' + type + '_city').val( data.sido );
+					$body.find( '#' + type + '-address-autocomplete' ).hide();
 				},
 				width : '100%',
 				height: '100%'
-			}).embed('postcode_form');
+			}).embed( type + '-address-autocomplete' );
 
-			wc_korea_postcode.$body.find('#postcode_form').show();
-		},
-
-		onClose: function() {
-			wc_korea_postcode.$body.find('#postcode_form').hide();
+			$body.find( '#' + type + '-address-autocomplete' ).show();
 		}
 
 	};
