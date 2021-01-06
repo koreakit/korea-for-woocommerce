ca9c05615748e6db268f7e2509957bb063209070
GREYS <4136890+jgreys@users.noreply.github.com>
2021-01-06 21:03:55 +0900

    Updated required & tested up versions

--- korea-for-woocommerce.php
+++ korea-for-woocommerce.php
@@ -6,8 +6,8 @@
  * Version: 1.1.1
  * Author: GREYS
  * Author URI: https://greys.co/
- * Requires at least: 4.9.0
- * Tested up to: 5.5.0
+ * Requires at least: 5.0.0
+ * Tested up to: 5.6.0
  * WC requires at least: 3.4.0
  * WC tested up to: 4.3.0
  *
