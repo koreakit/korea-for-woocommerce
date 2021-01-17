478d434d73cc2ed0c3cffdb746e3d716bf46538f
GREYS <4136890+jgreys@users.noreply.github.com>
2021-01-17 11:49:23 +0900

    Release 1.1.2

--- korea-for-woocommerce.php
+++ korea-for-woocommerce.php
@@ -3,11 +3,11 @@
  * Plugin Name: Korea for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/korea-for-woocommerce/
  * Description: WooCommerce Toolkit for Korean use.
- * Version: 1.1.1
+ * Version: 1.1.2
  * Author: GREYS
  * Author URI: https://greys.co/
  * Requires at least: 5.0.0
- * Tested up to: 5.5.0
+ * Tested up to: 5.6.0
  * WC requires at least: 3.4.0
  * WC tested up to: 4.9.0
  *
@@ -21,7 +21,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Define Constants.
-define( 'WC_KOREA_VERSION', '1.1.1' );
+define( 'WC_KOREA_VERSION', '1.1.2' );
 define( 'WC_KOREA_MAIN_FILE', __FILE__ );
 define( 'WC_KOREA_ABSPATH', dirname( __FILE__ ) );
 define( 'WC_KOREA_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
