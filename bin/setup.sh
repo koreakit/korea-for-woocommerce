5c232e553b64d5cf2d2dc5d7353704e159e8d0ed
John Greys <j.grzes@greys.co>
2021-03-22 07:13:47 +0900

    Added e2e tests on PR

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,40 +8,42 @@ echo "Setting up environment..."
 echo
 
 echo "Updating WordPress to the latest version..."
-wp-env run cli "wp core update --quiet"
+npx wp-env run cli "wp core update --quiet"
 
 echo "Updating the WordPress database..."
-wp-env run cli "wp core update-db --quiet"
+npx wp-env run cli "wp core update-db --quiet"
 
 echo "Updating the permalink structure"
-wp-env run cli "wp option update permalink_structure '/%postname%/'"
+npx wp-env run cli "wp option update permalink_structure '/%postname%/'"
 
 echo "Installing and activating WooCommerce..."
-wp-env run cli "wp plugin install woocommerce --activate"
+npx wp-env run cli "wp plugin install woocommerce --activate"
 
 echo "Creating customer account"
-wp-env run cli "wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer"
+npx wp-env run cli "wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer"
 
 echo "Adding basic WooCommerce settings..."
-wp-env run cli "wp option set woocommerce_store_address '123 Maple Street'"
-wp-env run cli "wp option set woocommerce_store_city 'Seoul'"
-wp-env run cli "wp option set woocommerce_default_country 'KR'"
-wp-env run cli "wp option set woocommerce_store_postcode '12345'"
-wp-env run cli "wp option set woocommerce_currency 'KRW'"
-wp-env run cli "wp option set woocommerce_product_type 'both'"
-wp-env run cli "wp option set woocommerce_allow_tracking 'no'"
+npx wp-env run cli "wp option set woocommerce_store_address '123 Maple Street'"
+npx wp-env run cli "wp option set woocommerce_store_city 'Seoul'"
+npx wp-env run cli "wp option set woocommerce_default_country 'KR'"
+npx wp-env run cli "wp option set woocommerce_store_postcode '12345'"
+npx wp-env run cli "wp option set woocommerce_currency 'KRW'"
+npx wp-env run cli "wp option set woocommerce_currency_pos 'right'"
+npx wp-env run cli "wp option set woocommerce_price_num_decimals '0'"
+npx wp-env run cli "wp option set woocommerce_product_type 'both'"
+npx wp-env run cli "wp option set woocommerce_allow_tracking 'no'"
 
 echo "Importing WooCommerce shop pages..."
-wp-env run cli "wp wc --user=admin tool run install_pages"
+npx wp-env run cli "wp wc --user=admin tool run install_pages"
 
 echo "Installing and activating the WordPress Importer plugin..."
-wp-env run cli "wp plugin install wordpress-importer --activate"
+npx wp-env run cli "wp plugin install wordpress-importer --activate"
 
 echo "Importing some sample data..."
-wp-env run cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip"
+npx wp-env run cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip"
 
 echo "Activating Korea for WooCommerce plugin..."
-wp-env run cli "wp plugin activate korea-for-woocommerce"
+npx wp-env run cli "wp plugin activate korea-for-woocommerce"
 
 echo
 echo "SUCCESS! You should now be able to access http://localhost:8888/wp-admin/"
--- tests/bin/setup.sh
+++ tests/bin/setup.sh
@@ -7,41 +7,37 @@ echo
 echo "Setting up tests environment..."
 echo
 
-echo "Updating WordPress to the latest version..."
-wp-env run tests-cli "wp core update --quiet"
-
-echo "Updating the WordPress database..."
-wp-env run tests-cli "wp core update-db --quiet"
-
 echo "Updating the permalink structure"
-wp-env run cli "wp option update permalink_structure '/%postname%/'"
+npx wp-env run tests-cli "wp rewrite structure '/%postname%/'"
 
-echo "Installing and activating WooCommerce..."
-wp-env run tests-cli "wp plugin install woocommerce --activate"
+echo "Activating WooCommerce..."
+npx wp-env run tests-cli "wp plugin activate woocommerce"
 
 echo "Creating customer account"
-wp-env run cli "wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer"
+npx wp-env run tests-cli "wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer"
 
 echo "Adding basic WooCommerce settings..."
-wp-env run tests-cli "wp option set woocommerce_store_address '123 Maple Street'"
-wp-env run tests-cli "wp option set woocommerce_store_city 'Seoul'"
-wp-env run tests-cli "wp option set woocommerce_default_country 'KR'"
-wp-env run tests-cli "wp option set woocommerce_store_postcode '12345'"
-wp-env run tests-cli "wp option set woocommerce_currency 'KRW'"
-wp-env run tests-cli "wp option set woocommerce_product_type 'both'"
-wp-env run tests-cli "wp option set woocommerce_allow_tracking 'no'"
+npx wp-env run tests-cli "wp option set woocommerce_store_address '123 Maple Street'"
+npx wp-env run tests-cli "wp option set woocommerce_store_city 'Seoul'"
+npx wp-env run tests-cli "wp option set woocommerce_default_country 'KR'"
+npx wp-env run tests-cli "wp option set woocommerce_store_postcode '12345'"
+npx wp-env run tests-cli "wp option set woocommerce_currency 'KRW'"
+npx wp-env run tests-cli "wp option set woocommerce_currency_pos 'right'"
+npx wp-env run tests-cli "wp option set woocommerce_price_num_decimals '0'"
+npx wp-env run tests-cli "wp option set woocommerce_product_type 'both'"
+npx wp-env run tests-cli "wp option set woocommerce_allow_tracking 'no'"
 
 echo "Importing WooCommerce shop pages..."
-wp-env run tests-cli "wp wc --user=admin tool run install_pages"
+npx wp-env run tests-cli "wp wc --user=admin tool run install_pages"
 
-echo "Installing and activating the WordPress Importer plugin..."
-wp-env run tests-cli "wp plugin install wordpress-importer --activate"
+echo "Activating the WordPress Importer plugin..."
+npx wp-env run tests-cli "wp plugin activate wordpress-importer"
 
 echo "Importing some sample data..."
-wp-env run tests-cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip"
+npx wp-env run tests-cli "wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip"
 
 echo "Activating Korea for WooCommerce plugin..."
-wp-env run tests-cli "wp plugin activate korea-for-woocommerce"
+npx wp-env run tests-cli "wp plugin activate korea-for-woocommerce"
 
 echo
 echo "SUCCESS! You should now be able to access http://localhost:8889/wp-admin/"
