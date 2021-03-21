c3b26267d6a563e58966912980cda42d8adc19e0
GREYS <4136890+jgreys@users.noreply.github.com>
2021-03-21 22:13:42 +0900

    Removed baseURL for visit function

--- tests/e2e/integration/wp-admin/wp-admin-settings-general.test.js
+++ tests/e2e/integration/wp-admin/wp-admin-settings-general.test.js
@@ -1,8 +1,7 @@
 describe( 'Korea for WooCommerce General Settings', function() {
-    const baseURL = Cypress.env('url');
 
 	before(function() {
-        cy.visit( baseURL + '/wp-login.php' );
+        cy.visit( '/wp-login.php' );
         cy.wait( 1000 );
         cy.get( '#user_login' ).type( Cypress.env( 'users' ).admin.username );
         cy.get( '#user_pass' ).type( Cypress.env( 'users' ).admin.password );
@@ -10,12 +9,12 @@ describe( 'Korea for WooCommerce General Settings', function() {
     });
 
     it( 'can update postcode settings', function() {
-		cy.visit( baseURL + '/wp-admin/admin.php?page=wc-settings&tab=integration&section=korea' );
+		cy.visit( '/wp-admin/admin.php?page=wc-settings&tab=integration&section=korea' );
 
 		cy.get( '#woocommerce_korea_postcode_yn' ).check();
 
 		cy.get( '.woocommerce-save-button' ).click();
 
-		cy.get('#message').should('have.class', 'updated');
+		cy.get( '#message' ).should( 'have.class', 'updated' );
 	} );
 } );
