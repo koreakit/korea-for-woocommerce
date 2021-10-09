8d2379a281fac8a514d50c02c12d28a465ad0f6d
Viktor Szépe <viktor@szepe.net>
2021-10-09 04:19:49 +0000

    Install PHPStan

---
+++ tests/phpstan/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+define( 'WC_KOREA_VERSION', '0.0.0' );
+define( 'WC_KOREA_MAIN_FILE', '.' );
+define( 'WC_KOREA_ABSPATH', '.' );
+define( 'WC_KOREA_PLUGIN_URL', '.' );
+define( 'WC_KOREA_PLUGIN_PATH', '.' );
